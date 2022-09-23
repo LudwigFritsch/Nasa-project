ea22982aa02cd77324bd9e430f57076036601a32
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-09-23 21:43:11 +0200

    Create data access functions in the planets and launches model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -28,16 +28,20 @@ function loadPlanetsData() {
   });
 }
 
-const isHabitablePlanet = (planet) => {
+function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
     planet["koi_insol"] > 0.36 &&
     planet["koi_insol"] < 1.11 &&
     planet["koi_prad"] < 1.6
   );
-};
+}
+
+function getAllPlanets() {
+  return habitablePlanets;
+}
 
 module.exports = {
   loadPlanetsData,
-  planets: habitablePlanets,
+  getAllPlanets,
 };
