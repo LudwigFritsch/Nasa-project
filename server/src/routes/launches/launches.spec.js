68d32945e7f459c11ff7b3d3db90c0dcb4a8b8c7
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-09-25 17:43:35 +0200

    Add test for error case in post /launches

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -24,6 +24,13 @@ describe("Test POST /launches", () => {
     target: "Kepler-186",
   };
 
+  const launchDataWithInvalidDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186",
+    launchDate: "not a date",
+  };
+
   test("It should respond with 201 created", async () => {
     const response = await request(app)
       .post("/launches")
@@ -37,6 +44,43 @@ describe("Test POST /launches", () => {
 
     expect(response.body).toMatchObject(launchDataWithoutDate);
   });
-  test("It should catch missing required property", () => {});
-  test("It should catch invalid dates", () => {});
+
+  test("It should check upcoming and success to be true", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const upcomingExpected = true;
+    const successExpected = true;
+    const upcomingResponse = response.body.upcoming;
+    const successResponse = response.body.success;
+
+    expect(upcomingResponse).toBe(upcomingExpected);
+    expect(successResponse).toBe(successExpected);
+  });
+
+  test("It should catch missing required property", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
+  });
 });
