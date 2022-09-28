3e59b4f8ed44e44524b835114e1960d6032904da
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-09-28 12:10:15 +0200

    Add mongodb connection to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "../.env" });
 
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
@@ -7,7 +9,19 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection
+  .once("open", () => {
+    console.log("MongoDB connection ready");
+  })
+  .on("error", (err) => {
+    console.error(err.message);
+  });
+
 const startServer = async () => {
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
