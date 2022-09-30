67a82be614447c552903e0d20ebc8cd84995b565
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-09-30 15:32:05 +0200

    Use mongo-service file to connect to mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,16 @@
 const http = require("http");
-const mongoose = require("mongoose");
 require("dotenv").config({ path: "../.env" });
 
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
+const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection
-  .once("open", () => {
-    console.log("MongoDB connection ready");
-  })
-  .on("error", (err) => {
-    console.error(err.message);
-  });
-
 const startServer = async () => {
-  mongoose.connect(process.env.MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
