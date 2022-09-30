a1a532b2bdf8cf071740c395846d09fb121135b4
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-09-30 21:29:41 +0200

    Add versioning to tests

--- server/src/routes/planets/planets.spec.js
+++ server/src/routes/planets/planets.spec.js
@@ -12,10 +12,10 @@ describe("Planets API", () => {
     await mongoDisconnect();
   });
 
-  describe("Test GET /launches", () => {
+  describe("Test GET /planets", () => {
     test("It should respond with 200 success", async () => {
       const response = await request(app)
-        .get("/planets")
+        .get("/v1/planets")
         .expect("Content-Type", /json/)
         .expect(200);
     });
