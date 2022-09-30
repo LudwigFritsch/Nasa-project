fa74288f1eecb54c4e1279f34110e7c0797484ab
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-09-30 15:32:21 +0200

    Use mongo-service file to connect to mongodb

--- server/src/routes/planets/planets.spec.js
+++ server/src/routes/planets/planets.spec.js
@@ -1,11 +1,19 @@
 const request = require("supertest");
-const app = require("../../app");
 
-describe("Test GET /planets", () => {
-  test("It should respond with 200 success", async () => {
-    const response = request(app)
-      .get("/planets")
-      .expect("Content-Type", /json/)
-      .expect(200);
+const app = require("../../app");
+const { mongoConnect } = require("../../services/mongo");
+
+describe("Planets API", () => {
+  beforeAll(async () => {
+    await mongoConnect();
+  });
+
+  describe("Test GET /planets", () => {
+    test("It should respond with 200 success", async () => {
+      const response = request(app)
+        .get("/planets")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
   });
 });
