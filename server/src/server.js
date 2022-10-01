161cc3d6a2797084fd60c623762fddd1f092cfd5
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-10-01 12:54:37 +0200

    Use async function to load launch on startup

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config({ path: "../.env" });
 
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadSingleLaunch } = require("./models/launches.model");
 const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT || 8000;
@@ -12,6 +13,8 @@ const server = http.createServer(app);
 const startServer = async () => {
   await mongoConnect();
   await loadPlanetsData();
+  await loadSingleLaunch();
+
   server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
   });
